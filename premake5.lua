6cc96c8a5746b9795f266ce0c079fc51ad935da0
RikalSiHurikan <ezyn69@gmail.com>
2021-10-18 12:31:17 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,41 +1,36 @@
+project "Box2D"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++11"
+	staticruntime "off"
 
---SOLUTION WORKSPACE
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-include "./vendor/premake/premake_customization/solution_items.lua"
-include "Dependencies.lua"
-
-workspace "Hurikan"
-	architecture "x86_64"
-	startproject "Storm"
-
-	configurations
+	files
 	{
-		"Debug",
-		"Release",
-		"Dist"
+		"src/**.h",
+		"src/**.cpp",
+		"include/**.h"
 	}
 
-	solution_items
+	includedirs
 	{
-		".editorconfig"
+		"include",
+		"src"
 	}
 
-	flags
-	{
-		"MultiProcessorCompile"
-	}
+	filter "system:windows"
+		systemversion "latest"
 
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}";
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
 
-group "Dependencies"
-	include "vendor/premake"
-	include "Hurikan/vendor/Box2D"
-	include "Hurikan/vendor/GLFW"
-	include "Hurikan/vendor/Glad"
-	include "Hurikan/vendor/ImGui"
-	include "Hurikan/vendor/yaml-cpp"
-group ""
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
 
-include "Hurikan"
-include "Sandbox"
-include "Storm"
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
